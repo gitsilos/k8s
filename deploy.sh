58606fe6ccfefa12bc1b76ccd1f8da7fde9b79a3
gitsilos <git.silos@gmail.com>
2019-12-25 13:34:56 -0500

    added

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
-docker build -t k8s/multi-client:latest -t k8s/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t k8s/multi-server:latest -t k8s/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t k8s/multi-worker:latest -t k8s/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+#docker build -t k8s/multi-client:latest -t k8s/multi-client:$SHA -f ./client/Dockerfile ./client
+#docker build -t k8s/multi-server:latest -t k8s/multi-server:$SHA -f ./server/Dockerfile ./server
+#docker build -t k8s/multi-worker:latest -t k8s/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push k8s/multi-client:latest
-docker push k8s/multi-server:latest
-docker push k8s/multi-worker:latest
+#docker push k8s/multi-client:latest
+#docker push k8s/multi-server:latest
+#docker push k8s/multi-worker:latest
 
-docker push k8s/multi-client:$SHA
-docker push k8s/multi-server:$SHA
-docker push k8s/multi-worker:$SHA
+#docker push k8s/multi-client:$SHA
+#docker push k8s/multi-server:$SHA
+#docker push k8s/multi-worker:$SHA
 
 
 kubectl apply -f k8s
